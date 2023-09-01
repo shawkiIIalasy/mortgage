4d7a95018688727f6943bc96d5e4513ccbb93194
Shawki Alassi <shawki.alassi@bitoasis.net>
2023-09-01 17:46:00 +0300

    Install Vue 3 | Inertia | Vuetify

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import DefaultLayout from '@/Layouts/DefaultLayout.vue';
+
+defineOptions({ layout: DefaultLayout });
+
+defineProps({
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="Mortgage" />
+
+    <div>
+        Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
+    </div>
+</template>
