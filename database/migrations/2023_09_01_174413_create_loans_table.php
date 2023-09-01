a5c3933febfcd32c6298514774882affb7705986
Shawki Alassi <shawki.alassi@bitoasis.net>
2023-09-02 00:39:56 +0300

    Database migrations

---
+++ database/migrations/2023_09_01_174413_create_loans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->double('amount');
+            $table->integer('yearly_term')->comment('Fixed Yearly Terms');
+            $table->double('interest_rate')->comment('Fixed Yearly Interest Rate');
+            $table->double('extra_payment')->nullable()->comment('Fixed Extra Payment');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
---
+++ database/migrations/2023_09_01_175553_create_loan_amortization_schedule_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loan_amortization_schedule', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('loan_id');
+            $table->integer('month_number');
+            $table->date('start_balance');
+            $table->double('amount')->comment('Monthly');
+            $table->double('principal_amount');
+            $table->double('interest_amount');
+            $table->double('end_balance');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table
+                ->foreign('loan_id')
+                ->references('id')
+                ->on('loans')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loan_amortization_schedule');
+    }
+};
